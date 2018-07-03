a2a6c9dd64ba23178076949623536ba1fe851f91
Gerard <geneseo2000@yahoo.com>
2018-07-03 14:34:33 -0400

    update

--- Networks/UDPClient.py
+++ Networks/UDPClient.py
@@ -5,7 +5,7 @@ serverPort = 12000
 
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 
-message = raw_input('Input lowercase sentence: ')
+message = input('Input lowercase sentence: ')
 clientSocket.sendto(message,(serverName, serverPort))
 modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
 
